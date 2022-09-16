3211eef1afc1468382b882436fc1be528ee30a7c
Timothy Allison <timothyjosephallison@gmail.com>
2022-09-16 07:40:54 -0600

    Inheritances.py

---
+++ inheritances.py
@@ -0,0 +1,13 @@
+class Roster(): #Parent/base class
+        Name = 'No Name Provided'
+        Weight = ''
+        Experience = ''
+        School = ''
+        
+class Athlete(Roster): #student class 1 
+        Position = ''
+        Grade = ''
+
+class Coach(Roster): #student class 2 
+        Penalties = ' '
+        Record = ''
